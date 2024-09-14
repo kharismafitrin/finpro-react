2c69d75ffd9042066ac827ea892ac2b6a5ad1b4f
kharismafitrin <kharismanisa14@gmail.com>
2024-09-14 17:10:14 +0700

    matiin logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import moviesReducer from "./moviesReducer";
 
 const rootReducer = combineReducers({
@@ -9,7 +9,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk, logger) // Tambahkan logger di sini
+  applyMiddleware(thunk) // Tambahkan logger di sini
 );
 
 export default store;
